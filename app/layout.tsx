af4878a2043e92adf39b43ade5500f11f94cb604
xya89 <xya89@sfu.ca>
2024-05-14 18:37:03 -0700

    fix bug for initial vercel deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './global.css'
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import { Navbar, Education, Experience, Projects } from './components/nav'
+import { Navbar } from './components/nav'
 
 // import { Analytics } from '@vercel/analytics/react'
 // import { SpeedInsights } from '@vercel/speed-insights/next'
