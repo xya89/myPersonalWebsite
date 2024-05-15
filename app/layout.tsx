360debf62883c4e542945631cc80d5374b1adcf7
xya89 <xya89@sfu.ca>
2024-05-14 20:38:52 -0700

    minor change on metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'hello',
+    template: '%s',
   },
   description: 'This is my portfolio.',
   openGraph: {
