3da76e10aebc39071b77aa91e35a9a96ae1c468f
xya89 <xya89@sfu.ca>
2024-05-16 15:52:29 -0700

    Remove all css styles for home page components for now.

--- app/components/education.tsx
+++ app/components/education.tsx
@@ -27,43 +27,43 @@ export function Education() {
             <div className="containter lg mx-auto space-y-6">
                 {educationData.map((edu, index) => (
                     // Card view for each education experience
-                    <div key={index} className="bg-white dark:bg-neutral-800 rounded-lg shadow-md overflow-hidden flex flex-col md:flex-row md:h-64 max-w-lg mx-auto md:max-w-4xl">
-                        <div className="p-6 flex flex-col md:flex-row">
+                    <div key={index} className="">
+                        <div className="">
                             {/* Image of the school */}
                             {edu.image && (
-                                <div className="w-full md:w-1/3 md:pr-6 flex-shrink-0">
-                                    <div className="relative pb-3/4 md:pb-0 md:h-full">
+                                <div className="">
+                                    <div className="">
                                         <img
                                             src={edu.image}
                                             alt={edu.school}
-                                            className="absolute inset-0 w-full h-full object-cover"
+                                            className=""
                                         />
                                     </div>
                                 </div>
                             )}
                             {/* School Info */}
-                            <div className={`p-6 flex flex-col justify-between ${edu.image ? 'md:w-1/2' : 'w-full'}`}>
+                            <div>
                                 {/* School Name */}
-                                <h1 className="font-bond text-2xl text-center mb-2">
+                                <h1>
                                     <a href={edu.slug}>
                                         {edu.school}
                                     </a>
                                 </h1>
                                 {/* Education Achievment */}
-                                <p className="text-sm text-sky-400 text-center mb-4">
+                                <p >
                                     {edu.title}
                                 </p>
                                 {/* time and location */}
-                                <div className="flex justify-between items-center text-sm mb-2">
-                                    <p className="text-neutral-600 dark:text-neutral-400">
+                                <div >
+                                    <p >
                                         {edu.year}
                                     </p>
-                                    <p className="text-neutral-600 dark:text-neutral-400">
+                                    <p >
                                         {edu.location}
                                     </p>
                                 </div>
                                 {/* Summary */}
-                                <p className="text-base text-gray-700 dark:text-gray-300 overflow-ellipsis truncate">
+                                <p>
                                     {edu.summary}
                                 </p>
                             </div>
