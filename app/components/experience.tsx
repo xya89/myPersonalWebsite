b9e1512c4feda37f3f1141fa701c582aa4fdff9e
xya89 <xya89@sfu.ca>
2024-05-18 09:55:47 -0700

    Implmenet Time line UI in <Experience/>

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -2,67 +2,80 @@ import Image from "next/image"
 import experienceData from 'app/data/experienceData.json'
 
 export function Experience() {
+
+    const yearsWihMostRecentProjects = {};
+    experienceData.forEach((exp) => {
+        const year = exp.duration.split('-')[0].trim();
+        if (!yearsWihMostRecentProjects[year]) {
+            yearsWihMostRecentProjects[year] = exp;
+        }
+    })
+
+
     return (
         <section className="flex flex-row justify-center w-full">
             <div className="containter lg:mx-auto space-y-6 justify-center w-full">
-                {experienceData.map((exp, index) => (
-                    // Card view for each experience item
-                    <div key={index} className="flex flex-col lg:flex-row overflow-hidden">
-                        {/* Time Line */}
-                        <div className="flex items-center absolute left-0 lg:relative lg:left-auto lg:w-1/12 mb-2 lg:mb-0">
-                            <div className="hidden lg:flex flex-col items-center">
-                                {index !== 0 && (
-                                    <div className="h-6 border-1-2 border-gray-300"></div>
-                                )}
-                                <div className="w-4 h-4 bg-gray-300 rounded-full border-2 border-gray-500"></div>
-                                {index !== 0 && experienceData.length - 1 && (
-                                    <div className="h-full border-1-2 border-gray-300"></div>
-                                )}
-                            </div>
-                            <p className="text-sm lg:text-center">{exp.duration.split('-')[0]}</p>
-                        </div>
-                        {/* Experience Info */}
-                        <div className="lg:w-1/2 p-4 flex flex-col justify-between lg:pr-8">
-                            {/* Title */}
-                            <h1 className="font-bold text-lg mb-1">
-                                {exp.title}
-                            </h1>
-                            {/* Organization Name */}
-                            <p className="text-gray-600 mb-1">
-                                {exp.orgName}
-                            </p>
-                            {/* Duration */}
-                            {/* Location */}
-                            <div className="flex justify-between text-gray-500 mb-2">
-                                <p>
-                                    {exp.duration}
-                                </p>
-                                <p>
-                                    {exp.location}
-                                </p>
-                            </div>
-                            {/* Summary */}
-                            <div className="text-gray-700">
-                                {exp.summary}
-                            </div>
-                        </div>
-                        {/* Image */}
-                        <a href={exp.slug} className="w-full lg:w-1/2">
-                            {exp.image && (
-                                <div>
-                                    <Image
-                                        src={exp.image}
-                                        alt={exp.title}
-                                        width={300}
-                                        height={300}
-                                        style={{ aspectRatio: '3/2', width: '100%', height: '100%', maxHeight: '300px' }}
-                                    />
-                                </div>
-                            )}
+                {experienceData.map((exp, index) => {
+                    const year = exp.duration.split('-')[0].trim();
+                    const isMostRecentProject = yearsWihMostRecentProjects[year] === exp;
 
-                        </a>
-                    </div>
-                ))}
+
+                    return (
+                        // Card view for each experience item
+                        <div key={index} className="flex flex-col lg:flex-row overflow-hidden">
+                            {/* Timeline */}
+                            <div className="w-1/12 flex flex-col items-center">
+                                {isMostRecentProject && (
+                                    <>
+                                        <div className="text-lg font-bold">{year}</div>
+                                        <div className="h-full w-px bg-gray-300"></div>
+                                    </>
+                                )}
+                            </div>
+                            {/* Experience Info */}
+                            <div className="lg:w-1/2 p-4 flex flex-col justify-between lg:pr-8">
+                                {/* Title */}
+                                <h1 className="font-bold text-lg mb-1">
+                                    {exp.title}
+                                </h1>
+                                {/* Organization Name */}
+                                <p className="text-gray-600 mb-1">
+                                    {exp.orgName}
+                                </p>
+                                {/* Duration */}
+                                {/* Location */}
+                                <div className="flex justify-between text-gray-500 mb-2">
+                                    <p>
+                                        {exp.duration}
+                                    </p>
+                                    <p>
+                                        {exp.location}
+                                    </p>
+                                </div>
+                                {/* Summary */}
+                                <div className="text-gray-700">
+                                    {exp.summary}
+                                </div>
+                            </div>
+                            {/* Image */}
+                            <a href={exp.slug} className="w-full lg:w-1/2">
+                                {exp.image && (
+                                    <div>
+                                        <Image
+                                            src={exp.image}
+                                            alt={exp.title}
+                                            width={300}
+                                            height={300}
+                                            style={{ aspectRatio: '3/2', width: '100%', height: '100%', maxHeight: '300px' }}
+                                        />
+                                    </div>
+                                )}
+
+                            </a>
+                        </div>
+                    )
+
+                })}
             </div>
         </section>
     )
