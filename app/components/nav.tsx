e8a7cfc9a676e44e0bd6eb68a5cd04c9877bde4c
xya89 <xya89@sfu.ca>
2024-05-18 16:14:40 -0700

    Collapsable nav bar on small screens

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,4 +1,7 @@
+'use client'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { CollapseIcon, CrossIcon } from 'public/icon'
 
 const navItems = {
   '/': {
@@ -20,25 +23,48 @@ const navItems = {
 }
 
 export function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState('');
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  const handleMenuItemClick = () => {
+    setIsOpen(false);
+  }
+
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              )
-            })}
+        <nav className="relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative" id="nav">
+          <div className="flex flex-row justify-between items-center px-4 py-2 lg:hidden">
+            <div className="text-lg font-bold">{currentPage}</div>
+            <button
+              onClick={toggleMenu}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded={isOpen ? 'true' : 'false'}
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <CollapseIcon />
+              ) : (
+                <CrossIcon />
+              )}
+            </button>
+          </div>
+          <div className={`lg:flex lg:flex-row space-x-0 pr-10 ${isOpen ? 'block' : 'hidden'} lg:block`}>
+            {Object.entries(navItems).map(([path, { name }]) => (
+              <Link
+                key={path}
+                href={path}
+                className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                onClick={handleMenuItemClick}
+              >
+                {name}
+              </Link>
+            ))}
           </div>
         </nav>
       </div>
--- public/icon.tsx
+++ public/icon.tsx
@@ -64,3 +64,42 @@ export function CalendarHero() {
     )
 }
 
+export function CollapseIcon() {
+    return (
+        <svg
+            className="block h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+            />
+        </svg>
+    )
+}
+
+export function CrossIcon() {
+    return (
+        <svg
+            className="block h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+            />
+        </svg>
+    )
+}
