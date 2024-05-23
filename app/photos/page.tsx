88bb7654126abe4a99869224436bcdd841c142a5
xya89 <xya89@sfu.ca>
2024-05-22 23:21:51 -0700

    remove bad approach in photos/page

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,38 +1,6 @@
-import fs from 'fs';
-import path from 'path';
+
 import ImageGallery from "app/components/imageGallery";
 
-// This function gets called at request time
-export async function getServerSideProps() {
-    try {
-        // Read the JSON file from the file system
-        const filePath = path.join(process.cwd(), 'app/data/imageSource.json');
-        const jsonData = fs.readFileSync(filePath, 'utf8');
-        const data = JSON.parse(jsonData);
-        console.log(data);
-
-        // Map data to the format expected by ImageGallery
-        const formattedImages = data.map((img, index) => ({
-            id: index,
-            src: img.src,
-            alt: img.alt,
-            width: 300,  // Set default width
-            height: 200, // Set default height
-        }));
-
-        // Pass data to the page via props
-        return { props: { images: formattedImages } };
-    } catch (error) {
-        console.error('Error reading JSON data:', error);
-        // Return an empty array if there's an error
-        return { props: { images: [] } };
-    }
-}
-
-export const metadata = {
-    title: 'Photo',
-    description: 'This is my photo',
-};
 
 export default function Page({ images }) {
     return (
