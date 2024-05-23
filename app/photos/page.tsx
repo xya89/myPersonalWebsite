3bc7982a8b4f5c518ef9e4efb1061969fd82bcb7
xya89 <xya89@sfu.ca>
2024-05-22 23:25:06 -0700

    fix bug in photo/page

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -2,7 +2,7 @@
 import ImageGallery from "app/components/imageGallery";
 
 
-export default function Page({ images }) {
+export default function Page() {
     return (
         <section>
             <p className='text-lg leading-relaxed my-20'>这是照片</p>
