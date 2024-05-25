41ce58c3dc77296f3d830df69afe19e821429a0c
xya89 <xya89@sfu.ca>
2024-05-24 20:45:09 -0700

    Deleted Test blogs

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -4,6 +4,7 @@ import ImageGallery from "app/components/imageGallery";
 export default function Page() {
     return (
         <section>
+            <h1 className="font-semibold text-2xl mb-8 mt-20 tracking-tighter">Photo</h1>
             <p className='text-lg leading-relaxed my-20'>这是照片</p>
             {/* image gallery */}
             {/* <ImageGallery /> */}
