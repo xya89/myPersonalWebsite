32f5ed1838b7380a5d289894b8da07219f63c8d6
xya89 <xya89@sfu.ca>
2024-05-30 08:56:13 -0700

    landing page design

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,8 @@ export default function Home() {
   const ref1 = useRef(null);
   const isVisible = useIsVisible(ref1);
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden">
+    <div className={`flex flex-col items-center justify-center w-screen h-screen overflow-hidden
+    transition-opactiy ease-in duration-700 ${isVisible ? "opacity-100" : "opacity-0"}`}>
       <div className="hidden w-screen h-px md:block" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
@@ -23,30 +24,36 @@ export default function Home() {
         color={{ r: 255, g: 0, b: 0 }}
       />
       <h1 ref={ref1}
-        className={`z-10 text-4xl sm:text-6xl md:text-8xl
-        transition-opactiy ease-in duration-700 ${isVisible ? "opacity-100" : "opacity-0"} mb-4
-        bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block
-        text-transparent bg-clip-text
+        className={`z-10 text-6xl sm:text-6xl md:text-8xl mb-4
+        bg-gradient-to-tr inline-block text-transparent bg-clip-text
+        from-red-300/20 via-neutral-600 to-stone-200/20 
+        dark:from-red-600/70 dark:via-zinc-600 dark:to-zinc-900
         `}>
         Hello
       </h1>
       <div className="hidden w-screen h-px md:block bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <h2 ref={ref1}
-        className={`z-10 mt-8 text-center break-normal text-2xl sm:text-3xl md:text-4xl
-      transition-opactiy ease-in duration-700 ${isVisible ? "opacity-100" : "opacity-0"}`}>
-        My name is Sean Yang, welcome to my website.
+        className={`z-10 mt-8 text-lg sm:text-xl md:text-2xl text-center break-normal
+        bg-gradient-to-tl inline-block text-transparent bg-clip-text
+        from-red-600/30 via-neutral-600/90 to-stone-200/30
+        dark:from-red-600/60 dark:via-zinc-500/80 dark:to-red-700
+      `}>
+        I'm Sean Yang, welcome to my website.
       </h2>
       <div className="my-16 text-center ">
         <nav className="my-16 ">
-          <ul className="flex items-center justify-center gap-4">
+          <ul className="flex items-center justify-center gap-4
+          bg-gradient-to-b text-transparent bg-clip-text 
+          from-zinc-700/70 via-zinc-500 to-zinc-700.70">
             {navigation.map((item) => (
               <Link
                 key={item.href}
                 href={item.href}
-                className="text-xl duration-500 text-zinc-500 hover:text-zinc-300"
+                className="text-xl duration-500 hover:text-zinc-800 dark:hover:text-zinc-300"
               >
                 {item.name}
               </Link>
+
             ))}
           </ul>
         </nav>
